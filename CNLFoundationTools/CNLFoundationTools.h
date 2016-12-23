2d8e32306b1db45c2e4da81af70ce7a316219b24
Igor Smirnov <dev_ios@ronin.pro>
2016-12-23 18:01:17 +0300

    change include of UIKit to Foundation

--- CNLFoundationTools/CNLFoundationTools.h
+++ CNLFoundationTools/CNLFoundationTools.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Complex Numbers. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CNLFoundationTools.
 FOUNDATION_EXPORT double CNLFoundationToolsVersionNumber;
