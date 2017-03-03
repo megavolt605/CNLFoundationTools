96e5790d8ae19367e6f80622da106d7252c6f596
Igor Smirnov <s@ronin.pro>
2017-03-03 14:01:24 +0300

    add date helpers

--- CNLFoundationTools/CNLFT+Date.swift
+++ CNLFoundationTools/CNLFT+Date.swift
@@ -56,6 +56,73 @@ public extension Date {
         return f.string(from: self)
     }
     
+    public var isToday: Bool {
+        let calendar = Calendar.autoupdatingCurrent
+        return calendar.isDateInToday(self)
+    }
+    
+    public var isYesterday: Bool {
+        let calendar = Calendar.autoupdatingCurrent
+        return calendar.isDateInYesterday(self)
+    }
+    
+    public var isTomorrow: Bool {
+        let calendar = Calendar.autoupdatingCurrent
+        return calendar.isDateInTomorrow(self)
+    }
+    
+    public var daysAgo: Int {
+        return daysEarlier(than: Date())
+    }
+    
+    public func daysEarlier(than date: Date) -> Int {
+        return abs(min(days(from: date), 0))
+    }
+    
+    public func earlierDate(_ date: Date) -> Date {
+        return (self.timeIntervalSince1970 <= date.timeIntervalSince1970) ? self : date
+    }
+    
+    public func days(from date: Date, calendar: Calendar? = nil) -> Int {
+        var calendarCopy = calendar
+        if calendar == nil {
+            calendarCopy = Calendar.autoupdatingCurrent
+        }
+        
+        let earliest = earlierDate(date)
+        let latest = (earliest == self) ? date : self
+        let multiplier = (earliest == self) ? -1 : 1
+        let components = calendarCopy!.dateComponents([.day], from: earliest, to: latest)
+        return multiplier*components.day!
+    }
+    
+    public var weekday: Int {
+        return component(.weekday)
+    }
+    
+    public func component(_ component: Calendar.Component) -> Int {
+        let calendar = Calendar.autoupdatingCurrent
+        return calendar.component(component, from: self)
+    }
+    
+    public func relative(to date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.doesRelativeDateFormatting = true
+        if isToday {
+            dateFormatter.timeStyle = .short
+            dateFormatter.dateStyle = .none
+        } else if isYesterday {
+            dateFormatter.timeStyle = .none
+            dateFormatter.dateStyle = .medium
+        } else if daysAgo < 6 {
+            return dateFormatter.weekdaySymbols[weekday - 1]
+        } else {
+            dateFormatter.timeStyle = .none
+            dateFormatter.dateStyle = .short
+        }
+        return dateFormatter.string(from: self)
+    }
+    
 }
 
 /// Additional date comparision operators
